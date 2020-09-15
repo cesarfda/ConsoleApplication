049a6c909eba2bf575e7815bfe6fc694e16ef3ed
Cesar <ara17008@byui.edu>
2020-09-15 14:15:14 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Cesar";
+            var location = "Rexburg, ID";
+
+            Console.WriteLine("My name is: " + name);
+            Console.WriteLine("I'm from: " + location);
+        }
+    }
+}

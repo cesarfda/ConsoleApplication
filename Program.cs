86fad59eccd43868544090a99be90edbda371022
Cesar <ara17008@byui.edu>
2020-09-17 13:56:15 -0600

    Added DaysToChristmas calculation

--- Program.cs
+++ Program.cs
@@ -15,6 +15,15 @@ namespace ConsoleApplication
 
             Console.WriteLine("My name is " + name);
             Console.WriteLine("I'm from " + location);
+            var today = DateTime.Now;
+            var date = DateTime.Now.ToShortDateString();
+            Console.WriteLine("Current date: {0}", date);
+
+            DateTime christmas = new DateTime(2020,12,25);
+            var daysToChristmas = (christmas.Date - today.Date).Days;
+
+            Console.WriteLine("Days until christmas from today: {0}", daysToChristmas);
+
             Console.ReadKey();
         }
     }
